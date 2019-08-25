b5ebc8731c13a54940094222e0daa94b0cea5ae3
fabran99 <fabran99@gmail.com>
2019-08-25 13:35:38 -0300

    add build

---
+++ build/precache-manifest.ea36d17e0ddea49093286fe4acf5d6d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727831e9b06eeb8becdf65e7ae6ee26f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b15fbbcb5ab60af8ffdb",
+    "url": "./static/css/main.e37d5f1e.chunk.css"
+  },
+  {
+    "revision": "e38b5c71333fd0047b93",
+    "url": "./static/js/2.9d270a0b.chunk.js"
+  },
+  {
+    "revision": "b15fbbcb5ab60af8ffdb",
+    "url": "./static/js/main.b683b690.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
